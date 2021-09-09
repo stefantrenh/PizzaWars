33a9c40ec9107978a86418df3fb7a5812cfac6e3
Stefan Trenh <Stefantrenh@hotmail.com>
2021-09-09 14:55:40 +0200

    got images from Api, need to refactor later on

--- PizzaWars.UI/wwwroot/js/app.js
+++ PizzaWars.UI/wwwroot/js/app.js
@@ -2,12 +2,49 @@
     ({
         el: '#app',
         data: {
+            pizzas: [],
+            pizzaUrl: "",
+            pizzaUrl2: ""
 
         },
         mounted() {
-
+            this.getData();
+            this.getPizzaUrl();
+            this.getPizzaUrl2();
         },
         methods: {
+            getData() {
+                axios.get('/Pizza/PizzaLists')
+                    .then(res => {
+                        this.pizzas = res.data;
+                        console.log(this.pizzas);
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    });
+            },
 
+                getPizzaUrl() {
+                    axios.get('https://foodish-api.herokuapp.com/api/images/pizza')
+                        .then(res => {
+                            this.pizzaUrl = res.data;
+                            
+                            console.log(this.pizzaUrl);
+                        })
+                        .catch(err => {
+                            console.log(err)
+                        });
+            },
+            getPizzaUrl2() {
+                axios.get('https://foodish-api.herokuapp.com/api/images/pizza')
+                    .then(res => {
+                        this.pizzaUrl2 = res.data;
+                        console.log(this.pizzaUrl);
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    });
+            },
+            
         }
     })
